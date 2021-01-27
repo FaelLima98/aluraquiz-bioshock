a4a607b978d36d6f695187c83b4016019c7368ea
Rafael <rlima6419@gmail.com>
2021-01-26 22:32:16 -0300

    Adicionado o Head do next.JS para linkar font do Google (Lobster)

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import Head from 'next/head';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
@@ -11,9 +14,9 @@ const GlobalStyle = createGlobalStyle`
     /* New styles */
     display: flex;
     flex-direction: column;
-    font-family: 'Lato', sans-serif;
+    font-family: 'Lobster', cursive;
     // Deixa branco no começo
-    color: ${({theme}) => theme.colors.contrastText};
+    color: ${({ theme }) => theme.colors.contrastText};
   }
   html, body {
     min-height: 100vh;
@@ -23,16 +26,21 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
-const theme = db.theme;
+`;
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
-    <>           
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
